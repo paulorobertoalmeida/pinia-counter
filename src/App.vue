fcedbd3c8d5d6b2af2dbc0cd4537d9c04ddcf783
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-08-17 15:32:34 +0200

    Setting up the project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  
+    <div class="app">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+      <RouterView />
+    </div>
+  
+</template>
+
+<style>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+
+const counter = ref(0);
+
+</script>
+
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <div class="count">{{ counter }}</div>
+    <div class="button">
+      <button>Decrease --</button>
+      <button>Increase ++</button>
+    </div>
+  </div>
+</template>
+
+<style>
+.counter {
+  font-size: 100px
+}
+</style>
